0de4065526805f793ae9bdcb19cb693fc8e4848b
LucasDiScipio <lucasdiscipio@hotmail.com>
2022-05-16 17:09:18 +0200

    low dimensional case - generating the data

--- Code/Data Generation.R
+++ Code/Data Generation.R
@@ -1,8 +1,16 @@
 # set working directory
-setwd("C:/Users/lucas/Documents/Facc/Analyse Multivariée/Travail Individuel/Projet R/Code")
+setwd("C:/Users/lucas/Documents/Facc/Analyse Multivariée/Travail Individuel/Multivariate-Analysis-Project")
+
+# clearing the environment and the console
+rm(list=ls())
+cat("\014")
 
 # Multivariate Normal Distribution
 library(MASS)
+
+# Multivariate Skew-Normal Distribution
+# install.packages("sn")
+# library(sn)
 
 # low dimension data (p <= 10)
 # ----------------------------
@@ -10,14 +18,22 @@ library(MASS)
 # 1.a)
 # X1
 p = 2
-n_1 = n_2 = 250
+
+n = 250 # n_1 = n_2 = 250
+
 Omega_tild = diag(1,p)
+
 alpha = matrix(5, p, 1)
+
 mu = matrix(0, p, 1)
+
 delta = sqrt(1+t(alpha) %*% Omega_tild %*% alpha)
 delta = (Omega_tild %*% alpha) / delta[1]
+
 mu_z = delta*sqrt(2/pi)
+
 cov_Z = Omega_tild - mu_z %*% t(mu_z)
-phi_p=mvrnorm(n_1, mu=mu_z, Sigma=cov_Z)
-  
+
+phi_p=mvrnorm(n, mu=mu_z, Sigma=cov_Z)
+
 # X2
